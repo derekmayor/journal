72ce8ed9a7ced453808fcb72fea1289cc173a871
Derek <derek@fabian4.site>
2026-02-04 19:48:51 +0800

    style: switch to Swiss Minimalist theme (Inter/Black&White)

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,10 +1,25 @@
 // .vitepress/theme/index.ts
 import DefaultTheme from 'vitepress/theme'
 import './custom.css'
+import mediumZoom from 'medium-zoom'
+import { onMounted, watch, nextTick } from 'vue'
+import { useRoute } from 'vitepress'
 
 export default {
   extends: DefaultTheme,
-  enhanceApp({ app }) {
-    // register global components if any
-  }
+  setup() {
+    const route = useRoute()
+    const initZoom = () => {
+      // Allow images to be zoomed
+      mediumZoom('[data-zoomable]', { background: 'var(--vp-c-bg)' })
+      mediumZoom('.vp-doc img', { background: 'var(--vp-c-bg)' })
+    }
+    onMounted(() => {
+      initZoom()
+    })
+    watch(
+      () => route.path,
+      () => nextTick(() => initZoom())
+    )
+  },
 }
