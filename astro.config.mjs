08b5157b2adf1d3d60088ee85e2a2cbdb9f3a59f
Derek <derek@fabian4.site>
2026-02-05 20:49:40 +0800

    fix: correctly install and configure Tailwind v4 via astro add

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,15 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://derekmayor.github.io',
   base: '/journal',
   integrations: [tailwind(), react(), sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
